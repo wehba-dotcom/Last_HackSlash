f3e81009c7a53a295c4f64f2e991f811983e1533
unknown <cph-wk7@cphbusiness.dk>
2022-05-01 22:02:54 +0200

    make the player win

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -9,12 +9,21 @@ public class PlayerScript : MonoBehaviour
     private int health = 100;
     public Text healthText;
     public Slider healthSlider;
+    public Camera myCamera;
+    private AudioSource s;
+    public AudioClip attackSound;
+
+    public void Start()
+    {
+        s = myCamera.GetComponent<AudioSource>();
+    }
 
     public void takeDamage()
     {
         health -= 20;
         healthText.text = "health " + health + "%";
         healthSlider.value = health / 100f;
+        s.PlayOneShot(attackSound);
 
         if(health==0)
         {
