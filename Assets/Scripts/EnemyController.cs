e3aae3163d3b82f0bfc070e7f285e75fa8939fa2
unknown <cph-wk7@cphbusiness.dk>
2022-06-21 23:26:22 +0200

    add the animation death to the enimy

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -61,7 +61,7 @@ public class EnemyController : MonoBehaviour
         if (other.CompareTag("Player"))
         {
            
-            transform.localScale= new Vector3(2,2,2);
+           anim.SetTrigger("die");
         }
     }
 }
